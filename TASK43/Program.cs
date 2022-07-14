d39793532096623b449e9ec6e6c70cfc593ffe89
Vadim <uvolkov@374@gmail.com>
2022-07-14 21:59:22 +0300

    Add Task43

---
+++ TASK43/Program.cs
@@ -0,0 +1,39 @@
+﻿double b1 = InputInt("Введите координаты точки b1: ");
+double k1 = InputInt("Введите координаты точки k1: ");
+double b2 = InputInt("Введите координаты точки b2: ");
+double k2 = InputInt("Введите координаты точки k2: ");
+
+
+double x = (k1 - k2)/(b2 - b1);
+double y = k1 * x + b1;
+
+if(b1 == b1)
+{
+    Console.WriteLine("Прямые паралельны!");
+}
+else
+{
+Console.WriteLine("Координаты точки пересечения прямых " + x + "," + y);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+double InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+
+
