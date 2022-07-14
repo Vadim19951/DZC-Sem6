94a381707a4392c42a595d153276c306270a942e
Vadim <uvolkov@374@gmail.com>
2022-07-14 21:57:14 +0300

    Add task41

---
+++ TASK41/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine("Введите числа через запятую: ");
+string originString = Convert.ToString(Console.ReadLine());
+
+int count = 0;
+ 
+int size = 0;
+
+for(int i = 0; i < originString.Length; i++)
+{
+         if(originString[i] == ',')
+         {
+           size++;
+           
+         }
+}
+            
+int j = 0;
+
+string tempString = "";
+
+int[] array = new int[size+1];
+
+for(int i = 0; i < originString.Length; i++)
+{
+        if(originString[i] != ',')
+         {
+           tempString += originString[i];
+         }
+         else
+         {
+            array[j] = Convert.ToInt32(tempString);
+            tempString = "";
+            j++;
+         }
+          if(i == originString.Length-1)
+         {
+            
+            array[j] = Convert.ToInt32(tempString);
+         }
+}
+
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0)
+    {
+       count++;
+    }
+}
+
+
+
+Console.WriteLine("Колличество чисел больше нуля равно " + count);
+
+
+
+
+
+
